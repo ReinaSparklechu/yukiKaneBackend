bb8570ef7c44fb7ad01bec9c8be83f858f63deb6
ReinaSparklechu <Sparklechus@gmail.com>
2022-09-06 13:13:24 +0800

    Backend environment for YukiKane app xD

---
+++ AuthServer/src/main/java/OAuth2/Yukikane/AuthServer/AuthServerApplication.java
@@ -0,0 +1,13 @@
+package OAuth2.Yukikane.AuthServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServerApplication.class, args);
+	}
+
+}
---
+++ AuthServer/src/test/java/OAuth2/Yukikane/AuthServer/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package OAuth2.Yukikane.AuthServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
