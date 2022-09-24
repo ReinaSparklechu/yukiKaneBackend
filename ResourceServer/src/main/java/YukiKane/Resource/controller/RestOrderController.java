04939ccbdec93d081dc3e2410444ca71ee4ea7e4
ReinaSparklechu <Sparklechus@gmail.com>
2022-09-24 22:16:04 +0800

    planning to change place order to receive only necessary information instead. Such as item list and outlet name. the rest can be processed on server. Server should then return order id back to client.

--- ResourceServer/src/main/java/YukiKane/Resource/controller/RestOrderController.java
+++ ResourceServer/src/main/java/YukiKane/Resource/controller/RestOrderController.java
@@ -3,6 +3,7 @@ package YukiKane.Resource.controller;
 
 import YukiKane.Resource.domain.Order;
 import YukiKane.Resource.repository.OrderRepository;
+import com.google.gson.annotations.JsonAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -19,15 +20,16 @@ public class RestOrderController {
     @PostMapping
     @ResponseStatus
     // todo: change to only receive array of items and outlet name.
-    public Order placeOrder(@RequestBody Order order, HttpServletRequest request) {
+    public Order placeOrder(@RequestBody String[] order, @RequestBody int[] qty, HttpServletRequest request) {
         var headers = request.getHeaders("*").asIterator();
         while (headers.hasNext()) {
             System.out.println(headers.next());
         }
         System.out.println("receiving order");
         System.out.println(order);
+        System.out.println(qty);
         //Todo:this should send a message to the receiving store's end, this should also update the order list in an outlet
-        return orderRepository.save(order).block();
+        return new Order();
     }
     @GetMapping("/{id}")
     public Order getOrder(@PathVariable String id) {
