1af8121afcdf2b09ba84c155c86701d9d611d612
ReinaSparklechu <Sparklechus@gmail.com>
2022-10-04 20:31:44 +0800

    Created order card for back client

--- back-client/src/components/Orders.vue
+++ back-client/src/components/Orders.vue
@@ -1,10 +1,27 @@
 <template>
-
+  <div>
+    <div>{{orderId}}</div>
+    <div>
+      <ul>
+        <li v-for="item in items" :key = item>{{item}}</li>
+      </ul>
+    </div>
+    <div v-if="!completed">
+      <button @click="completeOrder">Complete Order!</button>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Orders.vue"
+<script setup>
+import {ref} from "vue";
+import Axios from "axios"
+
+const orderId = ref("");
+const completed = ref(false);
+const items = ref([]);
+
+function completeOrder(){
+  Axios.post("http://localhost:8080/orders/complete", {'orderId': orderId});
 }
 </script>
 
